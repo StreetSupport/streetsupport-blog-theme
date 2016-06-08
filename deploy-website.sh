94ad554411cba89bfb18a4bdf9f833815eef6972
Vince Lee <vslee888@gmail.com>
2016-06-08 11:19:10 +0100

    copy dirs

--- deploy-website.sh
+++ deploy-website.sh
@@ -9,16 +9,32 @@ echo "*** Beginning Deployment ***"
 
 mkdir _dist
 
+echo "- copying php files"
 for f in *.php
 do
   cp $f _dist/{$f}
 done
 
+echo "- copying css files"
 for f in *.css
 do
   cp $f _dist/{$f}
 done
 
+echo "- copying required directories"
+REQ_DIRS[0]="images"
+REQ_DIRS[1]="inc"
+REQ_DIRS[2]="js"
+REQ_DIRS[3]="languages"
+REQ_DIRS[4]="layouts"
+REQ_DIRS[5]="template-parts"
+
+for dir in REQ_DIRS
+do
+  cp $dir _dist/{$dir}
+done
+
+echo "- _dist contents:"
 cd _dist
 ls
 
