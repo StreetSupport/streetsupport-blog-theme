4043e8edc3f82f16b0bff7a8fbf15ffe08a6f7a8
Vince Lee <vslee888@gmail.com>
2016-06-14 11:34:22 +0100

    change category names

--- template-parts/front-page.php
+++ template-parts/front-page.php
@@ -6,6 +6,7 @@ Template Name: front-page
 
 <?php get_header(); ?>
 
+
 <div class="block__content">
 		<?php query_posts('category_name=stories&showposts=1');
 			while ( have_posts() ) : the_post();
@@ -23,7 +24,7 @@ Template Name: front-page
 			</div>
 		</div>
 	</div>
-	<?php query_posts('category_name=about-homelessness&showposts=1');
+	<?php query_posts('category_name=articles&showposts=1');
 			while ( have_posts() ) : the_post();
 		$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
 		endwhile;
@@ -31,8 +32,8 @@ Template Name: front-page
 	<div class="main-image-block" style="background-image: url(<?php echo $src[0]; ?> ) !important;">
 		<div id="latest-about-homelessness-posts" class="container-post-excerpt">
 			<div class="text-wrap-div">
-				<h3><a class="read-more" href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">About Homelessness</a></h3>
-				<?php query_posts('category_name=about-homelessness&showposts=1');
+				<h3><a class="read-more" href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">Articles</a></h3>
+				<?php query_posts('category_name=articles&showposts=1');
 				while ( have_posts() ) : the_post();
 					get_template_part( 'template-parts/content-front-page-catexcerpt', 'page' );
 				endwhile; ?>
@@ -46,12 +47,12 @@ Template Name: front-page
 		<div id="latest-news-posts" class="front-page__news container-cat-titles">
 			<h1 class="h1">Latest News</h1>
 			<div class="front-page__news-list">
-				<?php query_posts('category_name=news&showposts=3');
+				<?php query_posts('category_name=latest-news&showposts=3');
 				while ( have_posts() ) : the_post();
 					get_template_part( 'template-parts/content-front-page-news', 'page' );
 				endwhile; ?>
 			</div>
-			<a href="<?php echo get_site_url (); ?>/category/news" class="btn btn--brand-e"><span class="btn__text">Read More News Stories</span></a>
+			<a href="<?php echo get_site_url (); ?>/category/latest-news" class="btn btn--brand-e"><span class="btn__text">Read More News Stories</span></a>
 		</div>
 	</div>
 </div>
