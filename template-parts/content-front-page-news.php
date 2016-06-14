38a2034a34b8830da5633079e4c79ba8ebe6e17d
Vince Lee <vslee888@gmail.com>
2016-06-14 15:22:51 +0100

    trying to tidy up front-page css. made new listing link around whole article

--- template-parts/content-front-page-news.php
+++ template-parts/content-front-page-news.php
@@ -12,17 +12,19 @@
 <article id="post-<?php the_ID(); ?>" class="front-page__news-item" <?php post_class(); ?>>
 		<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
 		?>
+	<a href="<?php echo esc_url( get_permalink() ) ?>">
 	<div class="front-page__news-item__news-image" style="background-image: url(<?php echo $src[0]; ?> ) !important;">
 		<?php
-			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
-		?></div>
+			the_title( '<h3 class="entry-title">', '</h3>' );
+		?>
+	</div>
 	<div class="news-spacing">
 	    <p><?php echo get_post_meta(get_the_ID(), 'front-page-snippet', true); ?></p>
 	    <p class="small front-page__news-item__meta">
-	      <span class="posted-on">Posted on <a href=""><?php echo the_time('d/m/y'); ?></a></span>
-	      <a class="read-more" href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">Read More</a>
+	      <span class="posted-on">Posted on <?php echo the_time('d/m/y'); ?></span>
 	    </p>
 	</div>
+	</a>
 </article>
 
 
