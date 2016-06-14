04574a92c40dc58d5d83a66d5abe132b292fda6d
Vince Lee <vslee888@gmail.com>
2016-06-14 14:51:06 +0100

    amended post meta

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -49,13 +49,13 @@ function ssnblog_entry_footer() {
 		/* translators: used between list items, there is a space after the comma */
 		$categories_list = get_the_category_list( esc_html__( ', ', 'ssnblog' ) );
 		if ( $categories_list && ssnblog_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in: %1$s', 'ssnblog' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'ssnblog' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 		}
 
 		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'ssnblog' ) );
 		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . esc_html__( ' Tagged: %1$s', 'ssnblog' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+			printf( '<span class="tags-links">' . esc_html__( ' Tagged %1$s', 'ssnblog' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 		}
 	}
 
