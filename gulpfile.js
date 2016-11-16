bb258b52f9c9f6e3c546616ec819ec343880d060
Vince Lee <vslee888@gmail.com>
2016-11-16 17:48:31 +0000

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
+
+var postcss      = require('gulp-postcss');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+
 var browserSync = require('browser-sync').create()
 
 gulp.task('sass', function () {
   return gulp.src('sass/style.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer({ browsers: ['last 3 versions', 'safari 8', 'ios 8', 'android 4', 'android 3'] }) ]))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('.'))
     .pipe(browserSync.reload({
       stream: true
