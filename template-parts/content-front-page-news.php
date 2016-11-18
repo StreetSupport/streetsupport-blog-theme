b5c0a13af97a50a3d17d0d6e4be57b472f0942b9
Vince Lee <vslee888@gmail.com>
2016-11-18 14:33:05 +0000

    move latest news titles out

--- template-parts/content-front-page-news.php
+++ template-parts/content-front-page-news.php
@@ -13,12 +13,12 @@
 		<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'front-page-news');
 		?>
 	<a href="<?php echo esc_url( get_permalink() ) ?>">
-	<div class="front-page__news-item__news-image" style="background: center center url(<?php echo $src[0]; ?> ) !important;">
-		<?php
-			the_title( '<h3 class="entry-title">', '</h3>' );
-		?>
+	<div class="front-page__news-item__news-image" style="background: top center url(<?php echo $src[0]; ?> ) !important;">
 	</div>
 	<div class="news-spacing">
+		<?php
+			the_title( '<h3 class="front-page__news-item__news-title">', '</h3>' );
+		?>
 	    <p class="front-page__news-item__snippet"><?php echo get_post_meta(get_the_ID(), 'front-page-snippet', true); ?></p>
 	    <p class="small front-page__news-item__meta">
 	      <span class="posted-on">Posted on <?php echo the_time('d/m/y'); ?></span>
