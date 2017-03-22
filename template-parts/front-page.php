a163ed659584928b793d4b459aa3b3b874231537
Ben Marley <benjaminjohnmarley@gmail.com>
2017-03-22 11:47:00 +0000

    Neatened up

--- template-parts/front-page.php
+++ template-parts/front-page.php
@@ -17,11 +17,10 @@ Template Name: front-page
   <?php query_posts('category_name=stories&showposts=1');
     while ( have_posts() ) : the_post();
   $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
-  endwhile;
+    endwhile;
   ?>
   	<div class="main-image-block" style="background-image: url(<?php echo $src[0]; ?> ) !important;">
       <div id="latest-stories-posts" class="container-post-excerpt">
-
 
 			<div class="text-wrap-div">
 				<h3><a class="read-more" href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">Latest Story</a></h3>
@@ -31,18 +30,16 @@ Template Name: front-page
 		</div>
 	</div>
 	<?php query_posts('category_name=articles&showposts=1');
-			while ( have_posts() ) : the_post();
-		$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
+		while ( have_posts() ) : the_post();
+		  $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
 		endwhile;
-		?>
+	?>
 	<div class="main-image-block" style="background-image: url(<?php echo $src[0]; ?> ) !important;">
 		<div id="latest-about-homelessness-posts" class="container-post-excerpt">
 			<div class="text-wrap-div">
 				<h3><a class="read-more" href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">Articles</a></h3>
-				<?php query_posts('category_name=articles&showposts=1');
-				while ( have_posts() ) : the_post();
-					get_template_part( 'template-parts/content-front-page-catexcerpt', 'page' );
-				endwhile; ?>
+				<?php get_template_part( 'template-parts/content-front-page-catexcerpt', 'page' );
+        ?>
 			</div>
 			<a class="main-image-block__more-link" href="<?php echo get_site_url(); ?>/category/articles">More Articles</a>
 		</div>
@@ -57,7 +54,8 @@ Template Name: front-page
 				<?php query_posts('category_name=latest-news&showposts=3');
 				while ( have_posts() ) : the_post();
 					get_template_part( 'template-parts/content-front-page-news', 'page' );
-				endwhile; ?>
+				endwhile;
+        ?>
 			</div>
 			<a href="<?php echo get_site_url (); ?>/category/latest-news" class="btn btn--brand-e"><span class="btn__text">Read More News Stories</span></a>
 		</div>
@@ -70,7 +68,7 @@ Template Name: front-page
 			<h1 class="h1 js-header footer-menu__heading">Subjects</h1>
 			<div class="front-page__comments-list js-item accordion__inner">
 				<?php get_template_part( 'template-parts/content-front-page-tags', 'page' );
-				 ?>
+        ?>
 			</div>
 		</div>
 		<div id="latest-categories" class="front-page__categories container-cat-titles offwhite-frontpage accordion__item">
@@ -84,7 +82,7 @@ Template Name: front-page
 			<h1 class="h1 js-header footer-menu__heading">Archive</h1>
 			<div class="front-page__archive-list js-item accordion__inner">
 				<?php get_template_part( 'template-parts/content-front-page-archives', 'page' );
-				 ?>
+        ?>
 			</div>
 		</div>
 	</div>
