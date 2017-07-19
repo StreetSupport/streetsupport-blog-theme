9a6a65d6ecbdc1d5ccd71e4e922f2e271f3133f9
Vince Lee <vince.lee@polyhatsoftware.co.uk>
2017-07-19 12:42:28 +0100

    added new google analytics tags

--- header.php
+++ header.php
@@ -17,6 +17,14 @@
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-PVPXCXT');</script>
+  <!-- End Google Tag Manager -->
+
   <script>
     (function(d) {
       var config = {
@@ -44,6 +52,12 @@
 </head>
 
 <body style="touch-action: manipulation;" <?php body_class(); ?>>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PVPXCXT"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    
     <div class="sticky">
         <header class="header header--desktop">
             <div class="container">
