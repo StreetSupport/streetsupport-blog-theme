f3238a4e4932403cbba227eb636ffdd1f3e05c78
Vince Lee <vslee888@gmail.com>
2018-03-29 19:16:44 +0100

    added link to jobs page

--- header.php
+++ header.php
@@ -71,6 +71,7 @@
                         <li class="nav__item nav__item--news nav__item--active"><a href="/">News</a></li>
                         <li class="nav__item nav__item--contact"><a href="https://streetsupport.net/contact/">Contact Us</a></li>
                         <li class="nav__item nav__item--contact"><a href="https://streetsupport.net/impact/">Impact</a></li>
+                        <li class="nav__item nav__item--contact"><a href="https://streetsupport.net/jobs/">Jobs</a></li>
                     </ul>
                 </nav>
             </div>
